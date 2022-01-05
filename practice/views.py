235b4c834e229d50534bcdb4c0b6faeebfd444e9
s2lee <therok.manager@gmail.com>
2022-01-05 09:57:34 +0900

    @api_view 에 주석달면서 공부

--- practice/views.py
+++ practice/views.py
@@ -13,27 +13,35 @@ from .serializers import BookSerializer
 def book_list(request):
     if request.method == 'GET':
         qs = Book.objects.all()
-        serializer = BookSerializer(qs, many=True)
-        return Response(serializer.data)
-    else:
+        serializer = BookSerializer(qs, many=True)  # 객체 목록 또는 쿼리셋을 serialize 할 때 , many=True 추가
+        # many => queryset 에 대응. many 없으면 instance 1개가 올 것으로 기대하고 있어 에러 발생함.
+        return Response(serializer.data)  # 클라이언트가 요청한 형태로 콘텐트를 렌더링함
+        # Response -> Http or Json 요청 콘텐트에 맞게 자동으로 렌더링
+    elif request.method == 'POST':
         serializer = BookSerializer(data=request.data)
+        #  장고와 달리 DRF 에서는 request 에서 데이터를 받을 때(request.data)
+        #  반드시 .is_valid() 여부를 체크해야 한다.
+        #  valid 하지 않을 때는 serializer.errors 를 리턴한다.
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+            return Response(serializer.data, status=201)  # 작성, 서버가 요청 접수 후 새 리소스를 작성함
+        return Response(serializer.errors, status=400)  # 잘못된 요청
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def book_detail(request, pk):
-    book = get_object_or_404(Book, pk=pk)
+    # 404 error 는 서버에서 요청한 리소스를 찾을 수 없을 때
+    book = get_object_or_404(Book, pk=pk)  # 단일 객체에서 get_object_or_404() 이용하기
     if request.method == 'GET':
         serializer = BookSerializer(book)
         return Response(serializer.data)
     elif request.method == 'PUT':
         serializer = BookSerializer(book, data=request.data)
+        # request 에서 data 를 받았으니 .is_valid() 필수
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        #  numeric HTTP status codes 보다 더 명확히 표현하고 싶을 때 status code 사용
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     else:
         book.delete()
