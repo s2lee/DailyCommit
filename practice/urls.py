386182b5665bb9d680a4e01588e2059b6946e56d
s2lee <therok.manager@gmail.com>
2022-01-09 19:55:27 +0900

    Mixins4

--- practice/urls.py
+++ practice/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('fbv_api/book/<int:pk>', views.book_detail),
     path('cbv_api/book', views.BookListAPIView.as_view()),
     path('cbv_api/book/<int:pk>', views.BookDetailAPIView.as_view()),
-    path('mixins/book/', views.BookListMixins.as_view()),
-    path('mixins/book/<int:pk>/', views.BookDetailMixins.as_view()),
+    path('mixins/book/<str:category>', views.BookListMixins.as_view()),
+    # path('mixins/book/<int:pk>/', views.BookDetailMixins.as_view()),
     path('generic/book', views.BookListGenericAPIView.as_view()),
     path('generic/book/<int:pk>', views.BookDetailGenericAPIView.as_view()),\
     path('viewset/', include(router.urls)),
