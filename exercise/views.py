63b2755fa671fcbaf9bab49bec3cb725ad20b885
s2lee <therok.manager@gmail.com>
2022-04-21 19:00:42 +0900

    fix views

--- exercise/views.py
+++ exercise/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-
 from .models import Book
 from .forms import BookForm
 
